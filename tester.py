67e1441241142ad58dca02d89e03797b9912f382
botmayank <botmayank@gmail.com>
2018-03-15 16:40:42 +0530

    Modified tester to read ADC values and print

--- tester.py
+++ tester.py
@@ -3,17 +3,30 @@ import os
 import time
 from datetime import datetime
 import RPi.GPIO as GPIO
+import spidev
 
 LOG_FILE = "/media/pi/MISHO/DATA/data_log.csv"
 
 PINS = [27, 22, 10, 9, 11, 5, 6, 13, 19, 26, 18,\
          23, 24, 25, 8, 7, 12, 16, 20, 21]
 
-SLEEP_TIME = 10 #Seconds
+CHANNELS = [0,1,2,3,4]
 
 GPIO.setmode(GPIO.BCM)
-
 mosfet_header = []
+
+SLEEP_TIME = 10 #Seconds
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
+def readadc(adcnum):
+    # read SPI data from the MCP3008, 8 channels in total
+    if adcnum > 7 or adcnum < 0:
+        return -1
+    r = spi.xfer2([1, 8 + adcnum << 4, 0])
+    data = ((r[1] & 3) << 8) + r[2]
+    return data
 
 for i in range(len(PINS)):
     GPIO.setup(PINS[i], GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
@@ -32,10 +45,24 @@ if os.stat(LOG_FILE).st_size == 0:
 while True:
     now = datetime.now()
     reading = []
+    setting = []
+
     for i in range(len(PINS)):
         reading.append(str(GPIO.input(PINS[i])))
 
+    for i in range(len(CHANNELS)):
+        setting.append(str(readadc(CHANNELS[i])))
+
+    config_reading = ",".join(setting) + "\n"
+
     csv_reading = str(now) + ", " + ",".join(reading) + "\n" 
+
+    print "-------------PINS READING------------------"
+    print csv_reading
+
+    print "------------CONFIG READING-----------------"
+    print config_reading
+
     file.write(csv_reading)
       
     file.flush()
